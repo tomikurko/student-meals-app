32c365309ad0a0baac4bdf046a616f8cf915b3ef
Rafe Ibrahim <rafei@dhcp-85-5.eduroam.aalto.fi>
2023-10-10 15:38:11 +0300

    integrated material ui and axios with frontend

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,10 @@
+import Button from '@mui/material/Button';
+
 export default function Home() {
-  return <h1>STUDENT MEALS</h1>;
+  return (
+    <>
+        <h1>STUDENT MEALS</h1>
+        <Button variant="contained">Hello world</Button>
+    </>
+  );
 }
