65e567716a7e80f2eb14f3a2f575fc94db29aea0
Tomi Kurko <tomi.kurko@iki.fi>
2023-11-21 13:30:49 +0200

    Refactor: postgres credentials from env variables

--- backend/deployment/deploy-backend-image.sh
+++ backend/deployment/deploy-backend-image.sh
@@ -2,12 +2,6 @@
 
 BACKEND_DIR=`pwd`/$(dirname $0)/..
 
-echo -e -n "\n>> PostgreSQL password: "
-read -s POSTGRES_PASSWORD
-echo -e "\n"
-
-sed -i "" "s/spring\.datasource\.password=.*/spring.datasource.password=${POSTGRES_PASSWORD}/" "${BACKEND_DIR}/src/main/resources/application.properties"
-
 cd ${BACKEND_DIR} && \
   echo -e "\n>> Building image ..\n" &&
   ./gradlew bootBuildImage --imageName=studentmeals/server && \
@@ -15,7 +9,5 @@ cd ${BACKEND_DIR} && \
   aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/o6b6o8s4 &&
   docker image tag studentmeals/server:latest public.ecr.aws/o6b6o8s4/studentmeals:latest && \
   docker image push public.ecr.aws/o6b6o8s4/studentmeals:latest
-
-sed -i "" "s/spring\.datasource\.password=.*/spring.datasource.password=studentmeals-secret/" "${BACKEND_DIR}/src/main/resources/application.properties"
 
 echo -e "\n>> Done.\n"
